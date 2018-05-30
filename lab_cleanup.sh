87d0c76463e9fdd810acb5a61a7602ee2f8aa3f3
James Anderton <janderton@burwood.com>
2018-05-30 10:56:56 -0500

    added initial docker-compose file for minio s3 compatible object store

--- lab_cleanup.sh
+++ lab_cleanup.sh
@@ -9,6 +9,7 @@
 ###################################################################################################################################
 export PWFILE=/srv/labinabox/passwords
 export NUM_TEAMS=`wc -l $PWFILE |awk '{print$1}'`
+export DOCKER_HOST=tcp://0.0.0.0:2375
 
 echo '**********Removing User Accounts and Home Folders*********'
 for id in `seq 1 $NUM_TEAMS`;do 
